ec6b6b55a7e98fbf229207ffdc6bb92993ac7412
Mae Huynh <myhuynh@umich.edu>
2017-01-18 10:41:36 -0500

    Adding gapminder_analysis.R to repository

---
+++ gapminder_analysis.R
@@ -0,0 +1,15 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "gapminder-FiveYearData.csv")
+gapminder <- read.csv("gapminder-FiveYearData.csv")
+
+
+#African Countries Life expectancies for 2007
+#get 2007 data
+is_2007 <- gapminder$year == 2007
+is_Africa <- gapminder$country == "Africa"
+africa_2007 <- gapminder[is_2007 & is_Africa,c("country", "lifeExp")]
+
+ggplot(data=gapminder, aes (x=year, y=lifeExp, color=continent)) + 
+  geom_point()
+
+ggplot(data=gapminder, aes (x=year, y=lifeExp, by=country, color=continent)) + 
+  geom_line(color="black") + geom_point (aes(size=gdpPercap))
